5134a8b86c9cbdc1958b0647d4fe3a411701d65c
MF <notsure@compuserv.com>
2017-11-01 15:56:44 -0500

    started working on database. created some classes based off previous lab assignments.

---
+++ app/src/main/java/com/example/hl4350hb/inspirationapp/DatabaseManager.java
@@ -0,0 +1,90 @@
+package com.example.hl4350hb.inspirationapp;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteConstraintException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ *
+ */
+
+public class DatabaseManager {
+
+    private Context context;
+    private SQLHelper helper;
+    private SQLiteDatabase db;
+
+    protected static final String DB_NAME = "inspires";
+    protected static final int DB_VERSION = 2;
+    protected static final String DB_TABLE = "inspirepics";
+    protected static final String ID_COL = "_id";
+    protected static final String NOTE_COL = "notes";
+    protected static final String IMG_COL = "imageid";
+    protected static final String DATE_COL = "datetaken";
+
+
+
+    public DatabaseManager(Context c) {
+        this.context = c;
+        helper = new SQLHelper(c);
+        this.db = helper.getWritableDatabase();
+    }
+
+    public void close() {
+        helper.close();
+    }
+
+    public Cursor getAllPics() {
+        Cursor cursor = db.query(DB_TABLE, null, null, null, null, null, DATE_COL);
+        return cursor;
+    }
+
+    public boolean addMovie(String note, String imageid, long date) {
+        ContentValues newProduct = new ContentValues();
+        newProduct.put(NOTE_COL, note);
+        newProduct.put(IMG_COL, imageid);
+        newProduct.put(DATE_COL, date);
+        try {
+            db.insertOrThrow(DB_TABLE, null, newProduct);
+            return true;
+        } catch (SQLiteConstraintException err) {
+            return false;
+        }
+    }
+
+    public boolean updateNote(int picID, String newNote) {
+        ContentValues changeNote = new ContentValues();
+        changeNote.put(NOTE_COL, newNote);
+
+        String where = ID_COL + " = ? ";
+        String[] whereArgs = {Integer.toString(picID)};
+        int rowsMod = db.update(DB_TABLE, changeNote, where, whereArgs);
+        if (rowsMod == 1) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public class SQLHelper extends SQLiteOpenHelper {
+        public SQLHelper(Context c) {
+            super(c,DB_NAME, null, DB_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            String createSQLbase = "CREAT TABLE %s ( %s INTEGER PRIMARY KEY AUTOINCREMENT, %s TEXT, %s TEXT, %s INTEGER UNIQUE )";
+            String createSQL = String.format(createSQLbase, DB_TABLE, ID_COL, NOTE_COL, IMG_COL, DATE_COL);
+            db.execSQL(createSQL);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + DB_TABLE);
+            onCreate(db);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/hl4350hb/inspirationapp/NoteCursorAdapter.java
@@ -0,0 +1,66 @@
+package com.example.hl4350hb.inspirationapp;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+
+/**
+ *
+ */
+
+public class NoteCursorAdapter extends CursorAdapter {
+
+    NoteChangedListener noteChangedListener;
+
+    private static final int ID_COL = 0;
+    private static final int NOTE_COL = 1;
+    private static final int IMG_COL = 2;
+    private static final int DATE_COL = 3;
+    private static final SimpleDateFormat dateFormatter = new SimpleDateFormat("M-dd-yyyy hh:mm:ss");
+
+    public NoteCursorAdapter(Context context, Cursor c, boolean autoRequery) {
+        super(context, c, autoRequery);
+
+        if (context instanceof NoteChangedListener) {
+            this.noteChangedListener = (NoteChangedListener) context;
+        } else {
+            throw new RuntimeException(context.toString() + " must implement NoteChangedListener");
+        }
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View v = inflater.inflate(R.layout.list_item, parent, false);
+        return v;
+    }
+
+    @Override
+    public void bindView(View view, Context context, final Cursor cursor) {
+        TextView notetxt = (TextView) view.findViewById(R.id.txt);
+        TextView datetxt = (TextView) view.findViewById(R.id.datetxt);
+        ImageView img = (ImageView) view.findViewById(R.id.img);
+
+        String note = cursor.getString(NOTE_COL);
+        notetxt.setText(note);
+        String imageid = cursor.getString(IMG_COL);
+        img.setImageURI(Uri.fromFile(new File(imageid)));
+        long datetaken = cursor.getLong(DATE_COL);
+        datetxt.setText(dateFormatter.format(datetaken));
+
+    }
+
+
+    interface NoteChangedListener {
+        void notifyNoteChanged(int picID, String newNote);
+    }
+}
